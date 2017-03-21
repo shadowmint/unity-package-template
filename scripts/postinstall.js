c887daabb193c74e70e520140170571c4744aa15
Douglas Linder <linderd@iinet.net.au>
2017-03-21 20:03:57 +0800

    update versions and scripts for template

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,19 +5,22 @@ var ncp = require('ncp');
 // Paths
 var src = path.join(__dirname, '..', 'src');
 var dir = path.join(__dirname, '..', '..', '..', 'Assets', 'packages');
+var ismodule = __dirname.split(path.sep).filter(function(i) { return i == 'node_modules'; }).length > 0;
 
 // Create folder if missing
-mkdirp(dir, function (err) {
-  if (err) {
-    console.error(err)
-    process.exit(1);
-  }
-
-  // Copy files
-  ncp(src, dir, function (err) {
+if (ismodule) {
+  mkdirp(dir, function (err) {
     if (err) {
-      console.error(err);
+      console.error(err)
       process.exit(1);
     }
+
+    // Copy files
+    ncp(src, dir, function (err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
   });
-});
+}
